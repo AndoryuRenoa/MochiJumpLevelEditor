e2d46fab585c8b4657fa53fce1187371e712d9d3
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-03-04 20:26:35 -0700

    Update HelloWorld.js

--- HelloWorld.js
+++ HelloWorld.js
@@ -2,4 +2,5 @@ alert ("Hello World using seperate directory .js file");
 
 /** I'm currently at the below url while studying JS and using firefox/scratchpad as my developer tools:
 * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration
+* http://creativejs.com/tutorials/create-an-ipad-optimised-game/index.html
 **/
