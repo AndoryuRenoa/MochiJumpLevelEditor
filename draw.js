c73d19c46fb2eb12955aef487a46ce9a18982991
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-03-08 07:55:55 -0700

    Create draw.js

---
+++ draw.js
@@ -0,0 +1,6 @@
+// this is step one of getting the level editor to work. I need to be able to draw within an element using the mouse
+
+let divElem = document.getElementById("div1");
+function draw(){
+
+}
