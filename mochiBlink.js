7433b7f4142336ebdfdf92bc599200b12efc9e7e
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-03-07 18:52:30 -0700

    Update mochiBlink.js

--- mochiBlink.js
+++ mochiBlink.js
@@ -1,21 +1,27 @@
 	let counter = 0;
+	let mouseExit = false;
 	function x(){
-    	var id = setInterval (frame, 1000);
-        function frame(){
-        if (counter == 50){
-        clearInterval(id);
-        }
-    	else if (counter%2 == 0){
-        document.getElementById("blink").src ="https://github.com/AndoryuRenoa/MochiJump/blob/master/blink.png?raw=true";
-        counter++;
-        }
-        else{
-        document.getElementById("blink").src ="https://github.com/AndoryuRenoa/MochiJump/blob/master/background.png?raw=true";
-        counter++;
-        }
+		if (mouseExit == true){
+			return;
+		}
+		else {
+			var id = setInterval (frame, 1000);
+			function frame(){
+			if (counter == 50){
+			clearInterval(id);
+			}
+			else if (counter%2 == 0){
+			document.getElementById("blink").src ="https://github.com/AndoryuRenoa/MochiJump/blob/master/blink.png?raw=true";
+			counter++;
+			}
+			else{
+			document.getElementById("blink").src ="https://github.com/AndoryuRenoa/MochiJump/blob/master/background.png?raw=true";
+			counter++;
+			}
+		}
 }
 }
 	function y(){
-		clearInterval(x.id);
-		// this currently does not stop function x() from continuing.
+		mouseExit = true;
+		// this doesn't stop function x() either.
 }
