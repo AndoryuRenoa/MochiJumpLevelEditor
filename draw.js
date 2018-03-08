b96a2b0f6c40f1cd55da47b94768f56ad4520532
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-03-08 09:35:19 -0700

    Update draw.js

--- draw.js
+++ draw.js
@@ -1,14 +1,6 @@
 // this is step one of getting the level editor to work. I need to be able to draw within an element using the mouse
 
-//https://stackoverflow.com/questions/322378/javascript-check-if-mouse-button-down
-
-/**
-*function mouse_position(e)
-*{
-*do stuff
-*}
-*<body onmousemove="mouse_position(event)"></body>
-*/
+//https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onmousemove_addeventlistener
 
 let divElem = document.getElementById("div1");
 function draw(){
