edf44719e8ffd0688b9603954813261cfeec5d69
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-03-13 18:36:09 -0600

    Update draw.js

--- draw.js
+++ draw.js
@@ -4,10 +4,13 @@
 
 //perhaps the best way to do this is with a keyboard command or with mouse down.
 
+//actually something like the below will work well for now. I just need some boolean triggers to market when it's on or off
+//and maybe some radio buttons for the size selection... I still need to figure out why the svg component is so much smaller
+//than the actual div container that it's in:
 
-  var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-	var x;
-	var y;
+    var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    var x;
+    var y;
     var mouseDown;
     var makeRect;
     var xmlns = "http://www.w3.org/2000/svg";
@@ -21,21 +24,18 @@
 		var output = "Coordinates : (" + x + "," + y +")";
 		document.getElementById("demoOutput").innerHTML = output;
 	}
-document.getElementById("paintBox").addEventListener("click", function(event){
+document.getElementById("paintBox").addEventListener("mousemove", function(event){
 		startDrawBox(event);
 	});
 	function startDrawBox(downClick){
-		startRectX = downClick.clientX-13;
-		startRectY = downClick.clientY-11;
+		startRectX = downClick.clientX-12;
+		startRectY = downClick.clientY-12;
         var newRect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
         newRect.setAttribute("x", startRectX);
         newRect.setAttribute("y", startRectY);
-        newRect.setAttribute("width", 50);
-        newRect.setAttribute("height", 50);
+        newRect.setAttribute ("width", 5);
+        newRect.setAttribute ("height", 5);
         newRect.setAttribute("fill", "#ffcc66");
         svg.appendChild(newRect);
         document.getElementById("paintBox").appendChild(svg);
       }
-
-
-
