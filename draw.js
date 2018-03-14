d3a7bd8210a3e2e29b30c0faaad4da916434063d
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-03-13 18:43:09 -0600

    Update draw.js

--- draw.js
+++ draw.js
@@ -8,26 +8,23 @@
 //and maybe some radio buttons for the size selection... I still need to figure out why the svg component is so much smaller
 //than the actual div container that it's in:
 
-    var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    var x;
-    var y;
+	var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+	var x;
+	var y;
     var mouseDown;
     var makeRect;
     var xmlns = "http://www.w3.org/2000/svg";
-   document.getElementById("paintBox").addEventListener("mousemove",function(event){
+   
+document.getElementById("paintBox").addEventListener("mousemove",function(event){
 		cordsFunction(event);
 	});
-	
-	function cordsFunction(e){
-		x = e.clientX;
-		y = e.clientY;
-		var output = "Coordinates : (" + x + "," + y +")";
-		document.getElementById("demoOutput").innerHTML = output;
-	}
+document.getElementById("paintBox").addEventListener("mousedown", mouseDn);
+document.getElementById("paintBox").addEventListener("mouseup", mouseUp);
 document.getElementById("paintBox").addEventListener("mousemove", function(event){
 		startDrawBox(event);
 	});
 	function startDrawBox(downClick){
+     if (mouseDown == true){
 		startRectX = downClick.clientX-12;
 		startRectY = downClick.clientY-12;
         var newRect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
@@ -39,3 +36,17 @@ document.getElementById("paintBox").addEventListener("mousemove", function(event
         svg.appendChild(newRect);
         document.getElementById("paintBox").appendChild(svg);
       }
+     }
+	function cordsFunction(e){
+		x = e.clientX;
+		y = e.clientY;
+		var output = "Coordinates : (" + x + "," + y +")";
+		document.getElementById("demoOutput").innerHTML = output;
+	}
+    function mouseDn(){
+    	mouseDown= true;
+        }
+    function mouseUp(){
+    	mouseDown= false;
+        }
+
