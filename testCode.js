2652757ecbe8dfef34a0003b2435f4c198d251cf
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-03-13 18:42:32 -0600

    Update testCode.js

--- testCode.js
+++ testCode.js
@@ -1,64 +1,46 @@
-<!DOCTYPE html>
-<html>
-<style>
-div.paintbox{
-	margin: 0px;
-	border: 5px solid blue;
-	position: relative;
-	width: 500px;
-	height: 300px;
-}
-div.words{
-	position: relative;	
-	left : 50%;
-}
-</style>
-<div class ="words">
-</div>
-<div class ="paintbox" id="paintBox">
-</div>
-<p id = "demoOutput">Cords go here</p>
-</body>
-<script>
-// hmmm I wonder why this doesn't work..
+//below code does not work because of js syntax... adding () after the function makes function not readable...
 
-	var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-	var x;
-	var y;
+
+
+var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    var x;
+    var y;
     var mouseDown;
     var makeRect;
     var xmlns = "http://www.w3.org/2000/svg";
-   document.getElementById("paintBox").addEventListener("mousemove",function(event){
+   
+document.getElementById("paintBox").addEventListener("mousemove",function(event){
 		cordsFunction(event);
 	});
-	
+document.getElementById("paintBox").addEventListener("mousedown", mouseDOWN());
+document.getElementById("paintBox").addEventListener("mouseup", mouseUp());
+document.getElementById("paintBox").addEventListener("mousemove", function(event){
+		startDrawBox(event);
+	});
+	function startDrawBox(downClick){
+     if (mouseDown == true){
+		startRectX = downClick.clientX-12;
+		startRectY = downClick.clientY-12;
+        var newRect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+        newRect.setAttribute("x", startRectX);
+        newRect.setAttribute("y", startRectY);
+        newRect.setAttribute ("width", 5);
+        newRect.setAttribute ("height", 5);
+        newRect.setAttribute("fill", "#ffcc66");
+        svg.appendChild(newRect);
+        document.getElementById("paintBox").appendChild(svg);
+      }
+     }
+        function mouseUp(){
+        mouseDown = false;
+        }
+
+        function mouseDOWN(){
+        mouseDown = true;
+        }
 	function cordsFunction(e){
 		x = e.clientX;
 		y = e.clientY;
 		var output = "Coordinates : (" + x + "," + y +")";
 		document.getElementById("demoOutput").innerHTML = output;
 	}
-document.getElementById("paintBox").addEventListener("mousedown", function(event){
-		startDrawBox(event);
-	});
-	function startDrawBox(downClick){
-		startRectX = downClick.clientX-13;
-		startRectY = downClick.clientY-11;
-        var newRect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-        newRect.setAttribute("x", startRectX);
-        newRect.setAttribute("y", startRectY);
-        newRect.setAttribute("fill", "#ffcc66");
-      }
-document.getElementById("paintBox").addEventListener("mouseup", function(event){
-		finishDrawBox(event);
-        });
-        function finishDrawBox(upClick){
-        	var finishRectX = upClick.clientX-13;
-        	var finishRectY = upClick.clientX-11;
-        	newRect.setAttribute("width", finishRectX-startRectX);
-        	newRect.setAttribute("height", finishRectY-startRectY);
-        	svg.appendChild(newRect);
-        	document.getElementById("paintBox").appendChild(svg);
-		}
-</script>
-</html>
