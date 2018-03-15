e2457e8c40aaef0f2a6c52bd6b31813272e95f75
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-03-14 20:25:11 -0600

    Delete HelloWorld.js

--- HelloWorld.js
+++
@@ -1,8 +0,0 @@
-alert ("Hello World using seperate directory .js file");
-
-/** I'm currently at the below url while studying JS and using firefox/scratchpad as my developer tools:
-* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration
-* http://creativejs.com/tutorials/create-an-ipad-optimised-game/index.html
-* surprisingly it took me a while to figure out the way JS works. Study how the DOM works, and JS interact, then create a simple
-* animation as step 1.
-**/
