20a5b926b7ddc45d011c814ef6824350733ca8c2
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-03-16 20:08:40 -0600

    Update mochiBlink.js

--- mochiBlink.js
+++ mochiBlink.js
@@ -1,10 +1,8 @@
-// was having issues with html/css layouts calling functions outside of when expected I think it is corrected now with proper css layout
-// the DOM and event handling seems to be very tricky with JS. onmouseleave seems to fire instantly for some reason for example.	
+// I'm just keeping this for fun	
 
 	var counter = 0;
 	var x;
 	var y;
-	var drawOutput;
 	function x(){
 		var id = setInterval (frame, 1000);
 		function frame(){
@@ -21,26 +19,5 @@
 			}
 		}
 	}
-	document.getElementById("paintBox").addEventListener("mousemove",function(event){
-		cordsFunction(event);
-	});
+
 	
-	function cordsFunction(e){
-		x = e.clientX;
-		y = e.clientY;
-		var output = "Coordinates : (" + x + "," + y +")";
-		document.getElementById("demoOutput").innerHTML = output;
-	}
-	
-	document.getElementById("paintBox").addEventListener("onmousedown",function(event){
-		captureStart(event);
-		draw();
-	});
-	function captureStart(e2);{
-		var xStart = e2.clientX;
-		var yStart = e2.clientY;
-	});
-	
-	function draw(){
-		// I think the solution here is to actually use SVG components	
-	}
