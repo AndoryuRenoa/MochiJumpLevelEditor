6c07641b82a8dedecd99952d63f92d6f876a5fe4
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-03-19 16:46:52 -0600

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,8 @@
-// the time has come to try to move data from the mJLE page to else where. In order to do this I will need a PHP script
-// as I am completely unfamiliar with PHP this test.php will serve as my starting point for this.
+/** the time has come to try to move data from the mJLE page to else where. In order to do this I will need a PHP script
+* as I am completely unfamiliar with PHP this test.php will serve as my starting point for this.
+* http://www.freewebmasterhelp.com/tutorials/phpmysql
+* I'm creating a local server using bitnami as a starting point */
 
-//http://www.freewebmasterhelp.com/tutorials/phpmysql
 
 <?
 phpinfo()
