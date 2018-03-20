c6c1d6e21387e07429da90deb13f14e1837fda5d
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-03-19 19:10:14 -0600

    Update beginning.php

--- beginning.php
+++ beginning.php
@@ -1,7 +1,8 @@
 /** the time has come to try to move data from the mJLE page to else where. In order to do this I will need a PHP script
 * as I am completely unfamiliar with PHP this test.php will serve as my starting point for this.
 * 
-* I'm creating a local server using bitnami as a starting point 
+* I'm creating a local server using bitnami as a starting point. Bitnami has a tonne of stuff inside it.
+* For now I'm just going to try to get the 
 *
 * Okay, well to get the following to work it actually needed to be inside of <html> tags for some reason. Not sure why exactly.. 
 * current location: http://www.freewebmasterhelp.com/tutorials/phpmysql/2 **/
