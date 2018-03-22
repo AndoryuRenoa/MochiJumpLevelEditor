4de00e8bda531ff9ff492f21741fba815faa81fb
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-03-22 07:37:54 -0600

    Update draw.js

--- draw.js
+++ draw.js
@@ -5,6 +5,7 @@
 * Current reading: https://www.w3schools.com/js/js_json_intro.asp
 * continued: https://blog.garstasio.com/you-dont-need-jquery/ajax/
 * continued: https://www.kirupa.com/html5/making_http_requests_js.htm
+* continued: https://stackoverflow.com/questions/7071544/post-from-xmlhttp-with-parameters
 */
 
 /** okay, i think a for(i=0; i<.length;i++) will work if I had the code to output to a database! time to learn how to do that!!!
