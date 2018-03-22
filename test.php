fa862dbf642448a6865e9043d348c64d007692c6
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-03-22 17:01:43 -0600

    Update test.php

--- test.php
+++ test.php
@@ -1,14 +1,10 @@
 // this will house the working code for using PHP to add to the mySQL database. Once complete it can be moved/renamed elsewhere
 
 /** the time has come to try to move data from the mJLE page to else where. In order to do this I will need a PHP script
-* as I am completely unfamiliar with PHP this test.php will serve as my starting point for this.
-* 
-* I'm creating a local server using bitnami as a starting point. Bitnami has a tonne of stuff inside it.
-* For now I'm just going to try to get the 
-*
-* Okay, well to get the following to work it actually needed to be inside of <html> tags for some reason. Not sure why exactly.. 
-* current location: http://www.freewebmasterhelp.com/tutorials/phpmysql/2 **/
-
+* okay so current questions:
+* 1. will SQL let you write a new table over an old one?
+* 2. should I break apart the PHP files depending on what they will do and call different PHP files from Javascript dependant on task?
+**/
 
 <?php
 $severname = ""//add server name
