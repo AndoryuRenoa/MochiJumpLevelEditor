23e0eb34a778a0d6f3f9f34c7946200562ce40d9
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-03-24 17:57:17 -0600

    Update and rename SendLevel.php to SaveLevel.php

---
+++ SaveLevel.php
@@ -0,0 +1,11 @@
+<? PHP
+$severname = ""//add server name
+$username = ""//add username
+$password = ""//add password
+$dbname = ""//add database! add database name
+  
+  //need to import JSON values and then send to DB
+  
+  
+  
+  ?>
