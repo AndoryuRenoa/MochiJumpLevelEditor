21ff26ad1abf3f20ca1607ecbfafa6b966ff3f87
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-03-25 17:19:24 -0600

    Update SaveLevel.php

--- SaveLevel.php
+++ SaveLevel.php
@@ -3,8 +3,9 @@ $severname = ""//add server name
 $username = ""//add username
 $password = ""//add password
 $dbname = ""//add database! add database name
+$tableName = "" // need to be sure that the table name and the JSON array are accessed properly
   
-  //need to import JSON values and then send to DB
+  //need to import JSON values, remove all previous rows from the current table, convert JSON to rows, then input new rows into table
   
   
   
