15c19524536f8f3e570ed90630eef0fd586500c2
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-03-27 17:42:25 -0600

    Update RetrieveLevel.php

--- RetrieveLevel.php
+++ RetrieveLevel.php
@@ -4,10 +4,9 @@ $severname = ""//add server name
 $username = ""//add username
 $password = ""//add password
 $dbname = ""//add database! add database name
-$tablename; //this will be sent over via js
+$tablename= htmlspecialchars($POST[q]);
 
-  //assign $tablename a value via POST then check if table exists if it does we need to update the svg in the DOM to contain values based
-  // on the contents of the table
+
   
   
   
