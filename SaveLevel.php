3b8b4e8d7d734131d520326f83d15cd0773a5f2e
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-03-28 15:37:34 -0600

    Update SaveLevel.php

--- SaveLevel.php
+++ SaveLevel.php
@@ -5,8 +5,13 @@ $password = ""//add password
 $dbname = ""//add database! add database name
 $tableName = "" // need to be sure that the table name and the JSON array are accessed properly
   
-  // I think the solution for tableName getting it's value here is to use include"tableMaker.php"
-  // will need to write a simple test to determine that TableMaker.php actually returns a value and then check "RetreiveTable.php"
+  /** I think the solution for tableName getting it's value here is to use include"tableMaker.php"
+  * will need to write a simple test to determine that TableMaker.php actually returns a value and then check "RetreiveTable.php"
+  * something like include (tableMaker.php);
+  * if (tableName == null){
+  * include(RetreiveTable.php);
+  *}
+  * for further reading: https://www.w3schools.com/php/php_includes.asp **/
   
   //need to import JSON values, remove all previous rows from the current table, convert JSON to rows, then input new rows into table
   
