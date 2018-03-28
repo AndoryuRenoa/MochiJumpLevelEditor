dd24629c5f537eb8f6986d770814d1d66ecfc494
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-03-28 16:47:01 -0600

    Update SaveLevel.php

--- SaveLevel.php
+++ SaveLevel.php
@@ -5,6 +5,10 @@ $password = ""//add password
 $dbname = ""//add database! add database name
 $tableName = "" // need to be sure that the table name and the JSON array are accessed properly
   
+$conn = new mysqli ($servername, $username, $password, $dbname);
+if ($conn->connect_error){
+   die("Connection failed: ".$conn->connect_error);
+}
   /** I think the solution for tableName getting it's value here is to use include"tableMaker.php"
   * will need to write a simple test to determine that TableMaker.php actually returns a value and then check "RetreiveTable.php"
   * something like include (tableMaker.php);
@@ -13,8 +17,9 @@ $tableName = "" // need to be sure that the table name and the JSON array are ac
   *}
   * for further reading: https://www.w3schools.com/php/php_includes.asp **/
   
-  //need to import JSON values, remove all previous rows from the current table, convert JSON to rows, then input new rows into table
+  //Need to remove previous rows from table
   
+  //Need to submit JSON value into rows for table
   
-  
+$conn-> close();  
   ?>
