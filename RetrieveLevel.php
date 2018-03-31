6fb8a754d5edaf87a2b9ae851f79b0ebe528aedd
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-03-31 15:24:48 -0600

    Update RetrieveLevel.php

--- RetrieveLevel.php
+++ RetrieveLevel.php
@@ -6,6 +6,11 @@ $password = ""//add password
 $dbname = ""//add database! add database name
 $tablename= htmlspecialchars($POST[q]);
 
+sessions_start();
+$_  SESSION['tablename']=$tablename;
+
+// still need to write how to retrieve this PHP, then how to format it so that it can be given to js and read by js (JSON)
+
 
   
   
