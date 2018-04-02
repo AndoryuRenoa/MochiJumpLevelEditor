254feb5afb0d8ed6caeff68725cc9986a5ac46d4
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-04-02 17:30:36 -0600

    Update SaveLevel.php

--- SaveLevel.php
+++ SaveLevel.php
@@ -11,14 +11,7 @@ $conn = new mysqli ($servername, $username, $password, $dbname);
 if ($conn->connect_error){
    die("Connection failed: ".$conn->connect_error);
 }
-  /** I think the solution for tableName getting it's value here is to use include"tableMaker.php"
-  * will need to write a simple test to determine that TableMaker.php actually returns a value and then check "RetreiveTable.php"
-  * something like include (tableMaker.php);
-  * if (tableName == null){
-  * include(RetreiveTable.php);
-  *}
-  * solution may also to use tags when posting via js i.e. "SaveLevel.php?q=t"
-  * for further reading: https://www.w3schools.com/php/php_includes.asp **/
+
   
   //Need to remove previous rows from table
   
