027b3d5c38a7108442dc8971f6196baa978d3471
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-04-15 08:34:50 -0600

    Update Tester.php

--- Tester.php
+++ Tester.php
@@ -1,5 +1,22 @@
 <?php
 
 // simple script to create test database, table, and two rows inside of it will be used to test retreival functionality
+$servername = "localhost";
+$username = "input";
+$password = "input";
+
+$conn = new mysqil($servername, $username, $password);
+
+$if ($conn->connect_error){
+  die("Connection failed: ".$conn->connect_error);
+}
+
+// create database
+$sql = "CREATE DATABASE levelDB";
+if ($conn->query($sql) === true){
+  echo "Database created successfully";
+}else{
+  echo "Error: "conn.error;
+}
 
 ?>
