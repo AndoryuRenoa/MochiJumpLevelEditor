ec28aceac89a8947a6a6172eb07ab8cfcc0499ca
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-09-06 16:26:06 -0600

    Update LevelRepository.java

--- MochiJumpLevelEditor2/src/main/java/com/mochijump/leveleditor/LevelRepository.java
+++ MochiJumpLevelEditor2/src/main/java/com/mochijump/leveleditor/LevelRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.mochijump.leveleditor.Level;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface LevelRepository extends CrudRepository<Level, Long> {
 	public List<Level> findByLevelName (String levelName);
+	
+	// In order to get a list of names you'll have to use the @Query annotation
+	// @Query ("SELECT level_name FROM level")
+	// public List<?> getLevelNames(); <- should work but want to check db table for correct column name
 
 }
