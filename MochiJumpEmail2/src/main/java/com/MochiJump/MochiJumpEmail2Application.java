948f23f574721cbfd07833d22b43821f21ed8255
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-09-25 18:32:51 -0600

    Created Simple Email App

---
+++ MochiJumpEmail2/src/main/java/com/MochiJump/MochiJumpEmail2Application.java
@@ -0,0 +1,12 @@
+package com.MochiJump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MochiJumpEmail2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MochiJumpEmail2Application.class, args);
+	}
+}
---
+++ MochiJumpEmail2/src/test/java/com/MochiJump/MochiJumpEmail2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.MochiJump;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MochiJumpEmail2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
