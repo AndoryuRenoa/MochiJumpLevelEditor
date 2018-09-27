43b120d1aaa6c59f55693e9755a139e1b2c17470
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-09-27 17:22:16 -0600

    minor update to include emailer in API

--- MochiJumpLevelEditor2/src/main/java/com/mochijump/leveleditor/MainController.java
+++ MochiJumpLevelEditor2/src/main/java/com/mochijump/leveleditor/MainController.java
@@ -101,4 +101,31 @@ public class MainController {
 		}
 	}
 	
+	@PostMapping(path="/message")
+	public @ResponseBody String sendMessage (@RequestBody String s) {
+		String output;
+		String output2="";
+		// this needs to send (s), research format;
+		try {
+			
+			URL url = new URL("http://localhost:8090/email/test");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP Error code : "
+                        + conn.getResponseCode());
+            }
+			InputStreamReader in = new InputStreamReader(conn.getInputStream());
+            BufferedReader br = new BufferedReader(in);
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+                output2 = output;
+            }
+            return output2;
+		}
+		catch (Exception e) {
+			return "unable to check new releases due to connection";
+		}	
+	}
+	}
+	
 }
