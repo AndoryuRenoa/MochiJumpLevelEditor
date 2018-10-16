df6b000e4873c9a7359eb85ecbc8a1bc7f90de5b
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-10-15 18:42:41 -0600

    added new entity

--- MochiJumpLevelEditor2/src/main/java/com/mochijump/leveleditor/LauchPage.java
+++ MochiJumpLevelEditor2/src/main/java/com/mochijump/leveleditor/LauchPage.java
@@ -2,14 +2,14 @@ package com.mochijump.leveleditor;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-//^ if proper dependencies aren't setup at the beginning they can be adjusted in pom.xml
 
 @Controller
 public class LauchPage {
 	@RequestMapping("/")
-	public String index() {
-		return"landing.html";
-	}
+	public String home(Model model) {
+              return "forward:/index.html";
+       }
 
 }
